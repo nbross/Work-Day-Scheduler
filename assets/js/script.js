fe8c8cb3378bb0d9fe140b099cf3efba110c90a6
nbross <nbross2001@gmail.com>
2022-03-25 16:46:30 -0400

    added the functionality for the date and time to be displayed on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
-// setup a function to display todays date and time with moment.js
+// setup a function to display todays date and time with moment.js -- done
 // setup a local storage system for the activities to be saved with a click of a button
 // setup a function to track the hours of the day
 // setup hour tracker to change the colors of the blocks if past, present, or future
 // also work in jquery with all this ^^^^^
+$(document).ready(function () {
+    // this will display the date and time on the page
+    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+    
+})
