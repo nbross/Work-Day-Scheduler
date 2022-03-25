4bd1411131a52e30ff2e954644ec523e38565513
nbross <nbross2001@gmail.com>
2022-03-25 16:32:28 -0400

    psuedo code in js for road map

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// setup a function to display todays date and time with moment.js
+// setup a local storage system for the activities to be saved with a click of a button
+// setup a function to track the hours of the day
+// setup hour tracker to change the colors of the blocks if past, present, or future
+// also work in jquery with all this ^^^^^
