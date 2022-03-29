588d5f7320ce2e7310143c7c56a0f4eaa3656370
nbross <nbross2001@gmail.com>
2022-03-28 21:23:05 -0400

    made getItems for each hour allowing the saved data to be brought to the page on a refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,26 @@ $(document).ready(function () {
     // this will display the date and time on the page
     $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
-    $("#save-Btn").on("click", function() {
-        
+    // click listener for the save button. allowing the activities to be saved
+    $(".saveBtn").on("click", function() {
+        console.log(this);
+        var text = $(this).siblings(".activity").val();
+        var time = $(this).parent().attr("id");
+
+        // these items are then set in the local storage 
+        localStorage.setItem(time, text);
     })
-})
+});
+
+// when page is refreshed this loads the saved data from the localStorage for each hour
+$("#hour8 .activity").val(localStorage.getItem("hour8"));
+$("#nine .activity").val(localStorage.getItem("nine"));
+$("#ten .activity").val(localStorage.getItem("ten"));
+$("#eleven .activity").val(localStorage.getItem("eleven"));
+$("#twelve .activity").val(localStorage.getItem("twelve"));
+$("#one .activity").val(localStorage.getItem("one"));
+$("#two .activity").val(localStorage.getItem("two"));
+$("#three .activity").val(localStorage.getItem("three"));
+$("#four .activity").val(localStorage.getItem("four"));
+$("#five .activity").val(localStorage.getItem("five"));
+$("#six .activity").val(localStorage.getItem("six"));
